cd10643106fbd69292285d456f4b2ec4747ddc8f
Sante96 <musixchannel01@gmail.com>
2026-03-05 02:46:20 +0100

    github action

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
